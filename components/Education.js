537d08f15a8dd7ce12b4d6ed6a9e3a6350c7b196
Rick Ono <rrono@mit.edu>
2023-08-30 17:58:58 -0400

    Add new classes, education, job

--- components/Education.js
+++ components/Education.js
@@ -1,65 +1,140 @@
-import React from 'react';
-import Image from 'next/image';
-import ClassCard from './ClassCard';
-import MITSvg from '../public/MIT.svg';
-import Andover from '../public/andover.png';
-import { Icon } from '@iconify/react';
+import React from "react";
+import Image from "next/image";
+import MITSvg from "../public/MIT.svg";
+import { Icon } from "@iconify/react";
 
 const Education = () => {
+  const [showAllClasses, setShowAllClasses] = React.useState(false);
   const courses = [
-    '6.172: Software Performance Engineering',
-    '6.031: Software Construction',
-    '6.046: Design and Analysis of Algorithms',
-    '18.404: Theory of Computation',
-    '6.036: Machine Learning',
-    '6.003: Signal Processing',
-    '6.004: Computation Structures',
-    '6.041: Probabilistic Systems Analysis',
-    '9.60: Machine Vision',
-    '9.53: Emergent Computations in Distributed Neural Circuits',
+    "6.5820: Computer Networks (current)",
+    "6.5830: Database Systems (current)",
+    "6.1060 [6.172]: Software Performance Engineering",
+    "6.5120: Formal Reasoning About Programs",
+    "6.S050: Programming Language Design",
+    "6.1120: Dynamic Computer Language Engineering",
+    "6.1800: Computer Systems Engineering",
+    "6.031: Software Construction",
+    "6.046: Design and Analysis of Algorithms",
+    "18.404: Theory of Computation",
+    "6.036: Machine Learning",
+    "6.003: Signal Processing",
+    "6.004: Computation Structures",
+    "6.041: Probabilistic Systems Analysis",
+  ];
+  const otherCourses = [
+    "9.60: Machine Vision",
+    "9.53: Emergent Computations in Distributed Neural Circuits",
+    "9.40: Neural Computation",
+    "9.66: Computational Cognitive Science",
+    "6.0001: Intro to CS Programming in Python",
+    "6.0002: Intro to Computational Thinking and Data Science",
+    "6.006: Introduction to Algorithms",
+    "6.042: Math for Computer Science",
+    "6.057: Introduction to MATLAB",
+    "6.148: Web Lab",
+    "6.S191: Deep Learning",
+    "18.03: Differential Equations",
+    "14.13: Psychology and Economics",
+    "14.01: Principles of Microeconomics",
+    "14.02: Principles of Macroeconomics",
+    "9.00: Psychological Science",
+    "5.12: Organic Chemistry",
+    "7.002: Fundamentals of Experimental Molecular Biology",
+    "7.03: Genetics",
+    "7.015: Introductory Biology",
+    "9.01: Introduction to Neuroscience",
+    "8.01: Physics I",
+    "8.02: Physics II",
+    "18.01 Calculus",
+    "18.022: Calculus",
+    "24.09: Minds and Machines",
+    "21L.580: Translations",
+    "12.011: Archaeological Science",
+    "21W.022: Writing and Experience",
   ];
   return (
     <div>
-      <h2 className='text-5xl font-bold'>Education</h2>
-      <div className='grid grid-cols-1 lg:grid-cols-5 w-full gap-10 py-16'>
-        <div className='col-span-2 flex items-center justify-center'>
-          <Image src={MITSvg} width='100' height='50' alt='MIT logo' />
+      <h2 className="text-5xl font-bold">Education</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-5 w-full gap-10 py-16">
+        <div className="col-span-2 flex items-center justify-center">
+          <Image src={MITSvg} width="100" height="50" alt="MIT logo" />
         </div>
-        <div className='col-span-3'>
-          <h3 className='text-3xl font-bold'>
+        <div className="col-span-3">
+          <h3 className="text-3xl font-bold">
             Massachusetts Institute of Technology
           </h3>
-          <h2 className='text-xl font-bold'>Major: Computer Science (6-3)</h2>
-          <h2 className='text-xl font-bold'>
-            Minor: Brain and Cognitive Sciences (9)
+          <h2 className="text-xl font-semibold italic">
+            Candidate for Master of Engineering in Computer Science and
+            Engineering (6-3)
           </h2>
-          <h2 className='text-xl font-bold'>GPA: 4.8</h2>
+          <h2 className="text-xl">Expected spring 2024, GPA: 5.0/5</h2>
         </div>
       </div>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col gap-1 rounded-lg drop-shadow-lg bg-white p-6'>
-          <h3 className='text-2xl font-bold mb-2'>Coursework</h3>
-          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-1'>
+      <div className="grid grid-cols-1 lg:grid-cols-5 w-full gap-10 pb-16">
+        <div className="col-span-2 flex items-center justify-center">
+          <Image src={MITSvg} width="100" height="50" alt="MIT logo" />
+        </div>
+        <div className="col-span-3">
+          <h3 className="text-3xl font-bold">
+            Massachusetts Institute of Technology
+          </h3>
+          <h2 className="text-xl font-semibold italic">
+            Bachelor of Science in Computer Science and Engineering (6-3)
+          </h2>
+          <h2 className="text-xl italic">
+            Minor in Brain and Cognitive Sciences (9)
+          </h2>
+          <h2 className="text-xl">2019-2023, GPA: 4.7/5</h2>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col gap-1 rounded-lg drop-shadow-lg bg-white p-6">
+          <h3 className="text-2xl font-bold mb-2">MIT Coursework</h3>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-1">
             {courses.map((course) => (
-              <p key={course} className='pl-2 pr-6 py-1 before:-z-50 relative'>
+              <p key={course} className="pl-2 pr-6 py-1 before:-z-50 relative">
                 {course}
                 <Icon
-                  icon='akar-icons:chevron-right'
-                  className='absolute text-white right-1 top-1/2 -translate-y-1/2'
+                  icon="akar-icons:chevron-right"
+                  className="absolute text-white right-1 top-1/2 -translate-y-1/2"
                 />
               </p>
             ))}
           </div>
+          {showAllClasses && (
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-1">
+              {otherCourses.map((course) => (
+                <p
+                  key={course}
+                  className="pl-2 pr-6 py-1 before:-z-50 relative"
+                >
+                  {course}
+                  <Icon
+                    icon="akar-icons:chevron-right"
+                    className="absolute text-white right-1 top-1/2 -translate-y-1/2"
+                  />
+                </p>
+              ))}
+            </div>
+          )}
+          {!showAllClasses && (
+            <button onClick={() => setShowAllClasses(!showAllClasses)}>
+              <div className="flex justify-center items-center gap-2 mt-4">
+                Show all
+                <Icon icon="akar-icons:chevron-down" />
+              </div>
+            </button>
+          )}
         </div>
-        <div className='col-span-2'></div>
+        <div className="col-span-2"></div>
       </div>
-      <div className='grid grid-cols-1 lg:grid-cols-5 w-full gap-10 py-16'>
-        <div className='col-span-2 flex items-center justify-center'>
-          <p className='font-serif text-blue-700 text-3xl'>ANDOVER</p>
+      <div className="grid grid-cols-1 lg:grid-cols-5 w-full gap-10 py-16">
+        <div className="col-span-2 flex items-center justify-center">
+          <p className="font-serif text-blue-700 text-3xl">ANDOVER</p>
         </div>
-        <div className='col-span-3'>
-          <h3 className='text-3xl font-bold'>Phillips Academy, Andover</h3>
-          <h2 className='text-xl font-bold'>GPA: 5.4/6, ACT: 36</h2>
+        <div className="col-span-3">
+          <h3 className="text-3xl font-bold">Phillips Academy, Andover</h3>
+          <h2 className="text-xl font-bold">GPA: 5.4/6, ACT: 36</h2>
         </div>
       </div>
     </div>
--- components/Jobs.js
+++ components/Jobs.js
@@ -1,10 +1,11 @@
-import React, { useState, useRef } from 'react';
-import { Icon } from '@iconify/react';
-import Image from 'next/image';
-import { motion, AnimatePresence } from 'framer-motion';
-import ZayaLogo from '../public/zayalogo.png';
-import MediaLogo from '../public/medialogo.svg';
-import BCHLogo from '../public/bch.png';
+import React, { useState, useRef } from "react";
+import { Icon } from "@iconify/react";
+import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
+import ZayaLogo from "../public/zayalogo.png";
+import RobinhoodLogo from "../public/robinhood.png";
+import MediaLogo from "../public/medialogo.svg";
+import BCHLogo from "../public/bch.png";
 
 const JobCard = ({
   company,
@@ -17,17 +18,17 @@ const JobCard = ({
   return (
     <div
       className={`cursor-pointer bg-white rounded-lg drop-shadow-lg p-6 flex flex-col sm:flex-row items-center gap-6 relative overflow-hidden before:absolute before:-inset-y-1 before:inset-x-32 ${borderColor} after:absolute after:inset-1 after:bg-white after:-z-10 before:-z-20 hover:before:inset-x-0 before:opacity-0 hover:before:opacity-100 before:transition-all after:transition-all after:rounded-md ${
-        isSelected && 'before:opacity-100 before:inset-x-0'
+        isSelected && "before:opacity-100 before:inset-x-0"
       }`}
       onClick={() => {
         setSelected(company);
       }}
     >
-      <Image src={image} alt='Media Lab logo' width='50' height='50' />
-      <div className='flex flex-col items-center sm:items-start'>
-        <h3 className='font-bold font-xl'>{company}</h3>
+      <Image src={image} alt="Media Lab logo" width="50" height="50" />
+      <div className="flex flex-col items-center sm:items-start">
+        <h3 className="font-bold font-xl">{company}</h3>
         {titles.map((title) => (
-          <p className='font-lg text-center sm:text-left' key={company}>
+          <p className="font-lg text-center sm:text-left" key={company}>
             {title}
           </p>
         ))}
@@ -40,26 +41,26 @@ const JobDetails = ({ company, details }) => {
   return (
     <AnimatePresence exitBeforeEnter>
       <motion.div
-        layoutId='jobdetail'
-        className='bg-white drop-shadow-lg rounded-md p-8'
+        layoutId="jobdetail"
+        className="bg-white drop-shadow-lg rounded-md p-8"
         animate={{ translateX: 0, translateY: 0, opacity: 1 }}
-        key='hello'
+        key="hello"
       >
-        <h2 className='font-semibold text-3xl'>{company}</h2>
-        <p className='italic'>{details.position}</p>
-        <div className='flex gap-4 items-center my-4'>
+        <h2 className="font-semibold text-3xl">{company}</h2>
+        <p className="italic">{details.position}</p>
+        <div className="flex gap-4 items-center my-4">
           {details.icons.map((icon) => (
             <Icon
               icon={icon}
               key={`${company}${icon}`}
-              height='30'
-              width='30'
+              height="30"
+              width="30"
             />
           ))}
         </div>
-        <p className='mb-4'>{details.description}</p>
-        <h4 className='font-bold mb-4 text-lg'>My Responsibilities</h4>
-        <ul className='flex flex-col gap-4'>
+        <p className="mb-4">{details.description}</p>
+        <h4 className="font-bold mb-4 text-lg">My Responsibilities</h4>
+        <ul className="flex flex-col gap-4">
           {details.responsibilities.map((responsibility) => (
             <li key={responsibility}>{responsibility}</li>
           ))}
@@ -70,85 +71,110 @@ const JobDetails = ({ company, details }) => {
 };
 
 const Jobs = () => {
-  const [selected, setSelected] = useState('Zaya Health');
+  const [selected, setSelected] = useState("Robinhood Markets");
   const experience = [
     {
-      company: 'Zaya Health',
-      image: ZayaLogo,
-      titles: ['Fullstack SWE Intern'],
-      borderColor: 'before:bg-indigo-400',
+      company: "Robinhood Markets",
+      image: RobinhoodLogo,
+      titles: ["Backend Software Engineer Intern"],
+      borderColor: "before:bg-green-500",
     },
     {
-      company: 'MIT Media Lab',
+      company: "Zaya Health",
+      image: ZayaLogo,
+      titles: ["Fullstack Software Engineer Intern"],
+      borderColor: "before:bg-indigo-400",
+    },
+    {
+      company: "MIT Media Lab",
       image: MediaLogo,
-      titles: ['Undergraduate Researcher', 'Mobile + Backend Engineer'],
-      borderColor: 'before:bg-black',
+      titles: ["Undergraduate Researcher", "Mobile + Backend Engineer"],
+      borderColor: "before:bg-black",
     },
     {
       company: "Boston Children's Hospital",
       image: BCHLogo,
-      titles: ['Computational Neuroscience + Machine Learning Intern'],
-      borderColor: 'before:bg-blue-800',
+      titles: ["Computational Neuroscience + Machine Learning Intern"],
+      borderColor: "before:bg-blue-800",
     },
   ];
+
   const jobDetails = {
-    'Zaya Health': {
-      position: 'Fullstack SWE Intern',
+    "Robinhood Markets": {
+      position: "Backend Software Engineer Intern",
       icons: [
-        'vscode-icons:file-type-js-official',
-        'vscode-icons:file-type-typescript-official',
-        'vscode-icons:file-type-reactjs',
-        'vscode-icons:file-type-node',
-        'vscode-icons:file-type-firebase',
+        "vscode-icons:file-type-go",
+        "vscode-icons:file-type-typescript-official",
+        "vscode-icons:file-type-reactjs",
+        "logos:postgresql",
       ],
+
       description:
-        'Zaya health is a company that strives to make high-quality mental healthcare more accessible to everyone by demystifying the insurance claims process.',
+        "I worked on the Internal Tooling Platform team at Robinhood and worked on two full-stack projects.",
       responsibilities: [
-        'Started integration with various insurance company FHIR-standard APIs to streamline the authentication process for accessing patient insurance data.',
-        'Built features using React and Express that are currently in production.',
-        'Created NodeJS Express endpoints to interface the client-side app with the Firestore database.',
+        "Built a tool for agents to attach a communications action to custom buttons, that sent templated email and app communications, empowering agents to automate communications with no engineering involvement.",
+        "Created a saved search feature from the ground up to allow agents to create and manage search filters for an internal case management tool.",
+        "Diagnosed and repaired bugs in the internal component library and database migration setup.",
       ],
     },
-    'MIT Media Lab': {
-      position: 'Undergraduate Researcher, Software Engineer',
+    "Zaya Health": {
+      position: "Fullstack SWE Intern",
       icons: [
-        'vscode-icons:file-type-reactjs',
-        'logos:python',
-        'bxl:flask',
-        'logos:postgresql',
-        'logos:c',
+        "vscode-icons:file-type-js-official",
+        "vscode-icons:file-type-typescript-official",
+        "vscode-icons:file-type-reactjs",
+        "vscode-icons:file-type-node",
+        "vscode-icons:file-type-firebase",
       ],
       description:
-        'I worked with the Conformable Decoders group at the MIT Media Lab on a smart-facemask project, which records biosignatures through various sensors on a typical surgical mask. We submitted a paper to Nature Electronics that is currently in review.',
+        "Zaya health is a company that strives to make high-quality mental healthcare more accessible to everyone by demystifying the insurance claims process.",
       responsibilities: [
-        'Created a mobile app in React Native to connect to the facemask using BLE for easy data collection.',
-        'Made a Flask server with a PostgreSQL database to store data from the facemask, and deployed the server to an Ubuntu VM using NGINX.',
-        'Assisted with programming the firmware of the DA-14585 microcomputer in C.',
+        "Started integration with various insurance company FHIR-standard APIs to streamline the authentication process for accessing patient insurance data.",
+        "Built features using React and Express that are currently in production.",
+        "Created NodeJS Express endpoints to interface the client-side app with the Firestore database.",
+      ],
+    },
+    "MIT Media Lab": {
+      position: "Undergraduate Researcher, Software Engineer",
+      icons: [
+        "vscode-icons:file-type-reactjs",
+        "logos:python",
+        "bxl:flask",
+        "logos:postgresql",
+        "logos:c",
+      ],
+      description:
+        "I worked with the Conformable Decoders group at the MIT Media Lab on a smart-facemask project, which records biosignatures through various sensors on a typical surgical mask. We submitted a paper to Nature Electronics that is currently in review.",
+      responsibilities: [
+        "Created a mobile app in React Native to connect to the facemask using BLE for easy data collection.",
+        "Made a Flask server with a PostgreSQL database to store data from the facemask, and deployed the server to an Ubuntu VM using NGINX.",
+        "Assisted with programming the firmware of the DA-14585 microcomputer in C.",
       ],
     },
     "Boston Children's Hospital": {
       position:
-        'Machine Learning + Computational Neuroscience Intern @ Stamoulis Laboratory',
+        "Machine Learning + Computational Neuroscience Intern @ Stamoulis Laboratory",
       icons: [
-        'vscode-icons:file-type-matlab',
-        'logos:python',
-        'logos:pytorch-icon',
+        "vscode-icons:file-type-matlab",
+        "logos:python",
+        "logos:pytorch-icon",
       ],
       description:
-        'The Computational Neuroscience Laboratory, led by Dr. Caterina Stamoulis researches brain dynamics and networks given high-dimensional data, and works to develop novel signal processing techniques.',
+        "The Computational Neuroscience Laboratory, led by Dr. Caterina Stamoulis researches brain dynamics and networks given high-dimensional data, and works to develop novel signal processing techniques.",
       responsibilities: [
-        'Used machine learning tools, such as the MATLAB Deep Learning Toolbox and PyTorch to perform classifications on the Adolescent Brain Cognitive Development dataset.',
-        'Developed a pipeline in MATLAB for performing machine learning classifications on arbitrary data without need for extensive machine learning knowledge.',
+        "Used machine learning tools, such as the MATLAB Deep Learning Toolbox and PyTorch to perform classifications on the Adolescent Brain Cognitive Development dataset.",
+        "Developed a pipeline in MATLAB for performing machine learning classifications on arbitrary data without need for extensive machine learning knowledge.",
       ],
     },
   };
+
   return (
     <>
-      <h2 className='text-5xl font-bold'>Work Experience</h2>
-      <div className='grid gap-8 lg:grid-cols-2 xl:grid-cols-3'>
-        <div className='relative flex flex-col gap-6'>
+      <h2 className="text-5xl font-bold">Work Experience</h2>
+      <div className="grid gap-8 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="relative flex flex-col gap-6">
           {experience.map((job) => (
-            <div key={job.company} className='relative'>
+            <div key={job.company} className="relative">
               <JobCard
                 {...job}
                 setSelected={setSelected}
@@ -157,7 +183,7 @@ const Jobs = () => {
             </div>
           ))}
         </div>
-        <div className='xl:col-span-2'>
+        <div className="xl:col-span-2">
           {Object.keys(jobDetails).map((job) => {
             return (
               job === selected && (
